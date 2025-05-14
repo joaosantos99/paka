b9b838a491bae89d37000c210a99b8e3c529244e
João Santos <68588265+joaosantos99@users.noreply.github.com>
2025-05-14 17:03:42 +0100

    feat: implement base to input component

---
+++ src/constants/InputConstants.js
@@ -0,0 +1,17 @@
+export const INPUTS = {
+  TEXT: 'text',
+  NUMBER: 'number',
+  EMAIL: 'email',
+  PASSWORD: 'password',
+  SELECTOR: 'selector',
+  CALENDAR: 'calendar',
+}
+
+export const INPUTS_TYPES = {
+  [INPUTS.TEXT]: 'text',
+  [INPUTS.NUMBER]: 'number',
+  [INPUTS.EMAIL]: 'email',
+  [INPUTS.PASSWORD]: 'password',
+  [INPUTS.SELECTOR]: 'select',
+  [INPUTS.CALENDAR]: 'calendar',
+};
